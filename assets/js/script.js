dca6a002e4e09b39fb12b274582be5371aef7f74
canslercp <canslercp@gmail.com>
2022-06-25 15:29:31 -0400

    Built html and css code. Started timer js.

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+// timer--------------------------------------->
+var timeEl = document.querySelector(".time");
+
+var secondsLeft = 60;
+
+function setTime() {
+  // Sets interval in variable
+  var timerInterval = setInterval(function() {
+    secondsLeft--;
+    timeEl.textContent = " " + secondsLeft;
+
+    if(secondsLeft === 0) {
+      // Stops execution of action at set interval
+      clearInterval(timerInterval);
+      // Calls function to create and append image
+      sendMessage();
+    }
+
+  }, 1000);
+}
+
+setTime();
