e9bc714b22f53f37ea0dc44809a6fb83ee26057c
canslercp <canslercp@gmail.com>
2022-06-25 20:43:47 -0400

    fixed timer and created event listener that starts the timer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,9 @@
-// timer--------------------------------------->
+//Click "start quiz" button to open quiz
+var startQuiz = document.querySelector(".purpButton");
+
+startQuiz.addEventListener("click", function(){
+
+// timer----doesnt work yet----------------------------------->
 var timeEl = document.querySelector(".time");
 
 var secondsLeft = 60;
@@ -7,16 +12,20 @@ function setTime() {
   // Sets interval in variable
   var timerInterval = setInterval(function() {
     secondsLeft--;
-    timeEl.textContent = " " + secondsLeft;
+    timeEl.textContent = secondsLeft;
 
     if(secondsLeft === 0) {
       // Stops execution of action at set interval
       clearInterval(timerInterval);
       // Calls function to create and append image
-      sendMessage();
+      //-->sendMessage();
     }
 
   }, 1000);
 }
 
 setTime();
+
+});
+
+
