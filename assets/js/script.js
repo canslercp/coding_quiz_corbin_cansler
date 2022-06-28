b8d0b461f3297a50739cf3c0537f540457d2ca80
canslercp <canslercp@gmail.com>
2022-06-28 09:25:16 -0400

    finished all question functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -26,15 +26,18 @@ startQuiz.addEventListener("click", function () {
 
   setTime();
 
-  //global variables for each question:
+  //global variables for each question function:
   var correctCounter = 0;
   var incorrectCounter = 0;
+  var ques1 = document.querySelector(".ques1");
   var ques2 = document.querySelector('.ques2');
+  var ques3 = document.querySelector('.ques3');
+  var ques4 = document.querySelector('.ques4');
+  var ques5 = document.querySelector('.ques5');
 
   //question one function
   function ques1Func() {
     var changeIntro = document.querySelector(".intro");
-    var ques1 = document.querySelector(".ques1");
 
     changeIntro.setAttribute('style', 'display: none');
     ques1.setAttribute('style', 'display: flex');
@@ -53,8 +56,7 @@ startQuiz.addEventListener("click", function () {
           correct.style.display = 'none';
         }, 2000); 
 
-        ques2.setAttribute('style', 'display:flex');
-        ques1.setAttribute('style', 'display:none');
+        ques2Func();
 
       //if incorrect answer is chosen
       }else{
@@ -67,8 +69,7 @@ startQuiz.addEventListener("click", function () {
           incorrect.style.display = 'none';
         }, 2000); 
 
-        ques2.setAttribute('style', 'display:flex');
-        ques1.setAttribute('style', 'display:none');
+        ques2Func();
       }
 
     }
@@ -76,7 +77,167 @@ startQuiz.addEventListener("click", function () {
   }
   ques1Func();
 
+  //question two function
+  function ques2Func() {
+
+    ques2.setAttribute('style', 'display:flex');
+    ques1.setAttribute('style', 'display:none');
+
+    // Listen for any clicks within the ques2 div
+    ques2.addEventListener("click", function (event) {
+      var answer = event.target;
+      //if correct answer is chosen
+      if (answer.matches('#ansC')){
+        var correct = document.querySelector(".correct");
+
+        correct.setAttribute('style', 'display:flex');
+        correctCounter++
+
+        setTimeout(() => {
+          correct.style.display = 'none';
+        }, 2000); 
+
+        ques3Func();
+
+      //if incorrect answer is chosen
+      }else{
+        var incorrect = document.querySelector(".incorrect");
+
+        incorrect.setAttribute('style', 'display:flex');
+        incorrectCounter++
+
+        setTimeout(() => {
+          incorrect.style.display = 'none';
+        }, 2000); 
+
+        ques3Func();
+      }
+
+    }
+    )
+  }
   
+  //question three function
+  function ques3Func() {
+
+    ques3.setAttribute('style', 'display:flex');
+    ques2.setAttribute('style', 'display:none');
+
+    // Listen for any clicks within the ques2 div
+    ques3.addEventListener("click", function (event) {
+      var answer = event.target;
+      //if correct answer is chosen
+      if (answer.matches('#ansD')){
+        var correct = document.querySelector(".correct");
+
+        correct.setAttribute('style', 'display:flex');
+        correctCounter++
+
+        setTimeout(() => {
+          correct.style.display = 'none';
+        }, 2000); 
+
+        ques4Func();
+
+      //if incorrect answer is chosen
+      }else{
+        var incorrect = document.querySelector(".incorrect");
+
+        incorrect.setAttribute('style', 'display:flex');
+        incorrectCounter++
+
+        setTimeout(() => {
+          incorrect.style.display = 'none';
+        }, 2000); 
+
+        ques4Func();
+      }
+
+    }
+    )
+  }
+   //question four function
+   function ques4Func() {
+
+    ques4.setAttribute('style', 'display:flex');
+    ques3.setAttribute('style', 'display:none');
+
+    // Listen for any clicks within the ques2 div
+    ques4.addEventListener("click", function (event) {
+      var answer = event.target;
+      //if correct answer is chosen
+      if (answer.matches('#ansD')){
+        var correct = document.querySelector(".correct");
+
+        correct.setAttribute('style', 'display:flex');
+        correctCounter++
+
+        setTimeout(() => {
+          correct.style.display = 'none';
+        }, 2000); 
+
+        ques5Func();
+
+      //if incorrect answer is chosen
+      }else{
+        var incorrect = document.querySelector(".incorrect");
+
+        incorrect.setAttribute('style', 'display:flex');
+        incorrectCounter++
+
+        setTimeout(() => {
+          incorrect.style.display = 'none';
+        }, 2000); 
+
+        ques5Func();
+      }
+
+    }
+    )
+  }
+  //question five function
+  function ques5Func() {
+
+    ques5.setAttribute('style', 'display:flex');
+    ques4.setAttribute('style', 'display:none');
+
+    // Listen for any clicks within the ques2 div
+    ques5.addEventListener("click", function (event) {
+      var answer = event.target;
+      //if correct answer is chosen
+      if (answer.matches('#ansA')){
+        var correct = document.querySelector(".correct");
+
+        correct.setAttribute('style', 'display:flex');
+        correctCounter++
+
+        setTimeout(() => {
+          correct.style.display = 'none';
+        }, 2000); 
+
+        endGameFunc();
+
+      //if incorrect answer is chosen
+      }else{
+        var incorrect = document.querySelector(".incorrect");
+
+        incorrect.setAttribute('style', 'display:flex');
+        incorrectCounter++
+
+        setTimeout(() => {
+          incorrect.style.display = 'none';
+        }, 2000); 
+
+        endGameFunc();
+      }
+
+    }
+    )
+  }
 });
 
+//This is the function for when the time runs out or all questions have been answered
+function endGameFunc () {
+
+}
 
