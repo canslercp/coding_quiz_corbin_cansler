ec8f30e50c0794abd135eb62164f107406700bf6
canslercp <canslercp@gmail.com>
2022-08-13 14:39:43 -0400

    added map function to hsscript

--- assets/js/hsscript.js
+++ assets/js/hsscript.js
@@ -2,8 +2,12 @@ var highScoresData = JSON.parse(localStorage.getItem('score'));
 console.log(highScoresData,'high scores data');
 
 var placeholder = document.querySelector('#placeholder');
-// placeholder.textContent = highScoresData.map
 
-// var map1 = highScoresData.map(x => placeholder.textContent = x);
-var map1 = highScoresData.map(x=>'${x.initials}','${x.score}')  
+var map1 = highScoresData.map(getHighScores).sort()
 placeholder.textContent = map1
+
+function getHighScores (item) {
+    const scores = item.initials + " - " + item.score + "\n";
+
+    return scores;
+}
